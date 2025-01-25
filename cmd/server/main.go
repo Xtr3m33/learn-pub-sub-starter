734eed36e6602f5f154ff309e453b98ba0c8d8f1
Xtr3m3 <2483998+Xtr3m33@users.noreply.github.com>
2025-01-25 21:39:34 +1100

    initial setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const connectionString = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("error conencting to rabbitmq server: %v", err)
+	}
+	defer connection.Close()
+	fmt.Println("Successfully connected to rabbitmq server")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down Peril server...")
+	os.Exit(0)
 }
