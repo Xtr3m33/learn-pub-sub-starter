25200fc3c83a992c720be3d18bf2d7e07f15415c
Xtr3m3 <2483998+Xtr3m33@users.noreply.github.com>
2025-01-26 16:08:23 +1100

    added initial server logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -14,6 +16,7 @@ const connectionString = "amqp://guest:guest@localhost:5672/"
 func main() {
 	fmt.Println("Starting Peril server...")
 
+	// Open connection to rabbitmq server
 	connection, err := amqp.Dial(connectionString)
 	if err != nil {
 		log.Fatalf("error conencting to rabbitmq server: %v", err)
@@ -21,7 +24,25 @@ func main() {
 	defer connection.Close()
 	fmt.Println("Successfully connected to rabbitmq server")
 
-	// wait for ctrl+c
+	msgChannel, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("error opening channel for rabbitmq server: %v", err)
+	}
+
+	//TODO - Testing
+	err = pubsub.PublishJSON(
+		msgChannel,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Fatalf("error sending message on channel: %v", err)
+	}
+
+	// Wait for ctrl+c (exit)
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	<-signalChan
