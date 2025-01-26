a39d06e0ae4743641b38bf0d3a44609b45e23bda
Xtr3m3 <2483998+Xtr3m33@users.noreply.github.com>
2025-01-26 16:36:27 +1100

    initial client logic

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const connectionString = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	// Open connection to rabbitmq server
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("error conencting to rabbitmq server: %v", err)
+	}
+	defer connection.Close()
+	fmt.Println("Successfully connected to rabbitmq server")
+
+	// Get username
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("error getting username: %v", err)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(
+		connection,
+		routing.ExchangePerilDirect,
+		fmt.Sprintf("%s.%s", routing.PauseKey, username),
+		routing.PauseKey,
+		pubsub.Transient,
+	)
+	if err != nil {
+		log.Fatalf("error declaring queue: %v", err)
+	}
+
+	// Wait for ctrl+c (exit)
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down Peril server...")
+	os.Exit(0)
 }
